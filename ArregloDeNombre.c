87295138ecbbf837a2e478cf50fa5552f2565611
Jimena Aybar <jimeaybar1999@gmail.com>
2026-04-14 15:47:45 -0300

    Punto 1

--- ArregloDeNombre.c
+++ ArregloDeNombre.c
@@ -3,8 +3,35 @@
 #include <string.h>
 
 int main(){
+    int cant=5, largo=100;
+    char *nombres[cant];
+    char nomb[largo];
+    printf("Ingrese 5 nombres:\n ");
 
-    char nombre[20];
+    for (int i = 0; i < cant; i++){
+        printf("Nombre %d:\n ", i+1);
+        fgets(nomb, largo, stdin);
+        nomb[strcspn(nomb, "\n")];
+
+        int longitud= strlen(nomb);
+        nombres[i]= (char *)malloc(longitud * sizeof(char));
+        
+        strcpy(nombres[i],nomb);
+    
+    }
+
+    MostrarPersonas(nombres, cant);
+
+    for (int i=0; i<cant; i++){
+        free(nombres[i]);
+    }
 
     return 0;
+}
+
+void MostrarPersonas(char *vector[],int tam){
+    printf("Nombres almacenados: \n");
+    for(int i=0; i< tam; i++){
+        printf("Persona %d: %s\n", i+1 , vector[i]);
+    }
 }
