7bc6d7dfeba0f26bc72a78a25cf6dab773a9612c
jimeaybar <jimeaybar1999@gmail.com>
2026-04-24 21:46:56 -0300

    funcion busca-nombre

--- ArregloDeNombre.c
+++ ArregloDeNombre.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAMA 100
+
+void MostrarPersonas(char *nom[] , int cant);
+
+void BuscarNombrePorId(char *nom[], int can , int numEntero);
 
 int main(){
-    int cant=5, largo=100;
-    char *nombres[cant];
-    char nomb[largo];
-    printf("Ingrese 5 nombres:\n ");
 
-    for (int i = 0; i < cant; i++){
-        printf("Nombre %d:\n ", i+1);
-        fgets(nomb, largo, stdin);
-        nomb[strcspn(nomb, "\n")];
 
-        int longitud= strlen(nomb);
-        nombres[i]= (char *)malloc(longitud * sizeof(char));
-        
-        strcpy(nombres[i],nomb);
+
+    int cantNombres = 5;
+    char buff[TAMA];
+    char *nombres[cantNombres];
     
+    for (int i = 0; i < cantNombres; i++)
+    {
+        printf("ingrese el nombre numero %d: ", i + 1);
+        gets(buff);
+        nombres[i] = (char*)malloc(strlen(buff) + 1);
+        strcpy(nombres[i] , buff);
+        
+        
     }
+    
+    MostrarPersonas(nombres , cantNombres);
 
-    MostrarPersonas(nombres, cant);
+    int numfuncion;
+    printf("\nPara buscar nombre por id ingrese el numero 1:\n");
+    printf("Para buscar nombre por clave ingrese el numero 2:");
+    scanf("%d", &numfuncion);
+    if (numfuncion == 1)
+    {   int id;
 
-    for (int i=0; i<cant; i++){
-        free(nombres[i]);
+        printf("\ningrese un numero entero positivo:");
+        scanf("%d", &id);
+        BuscarNombrePorId(nombres, cantNombres , id);
     }
 
     return 0;
 }
 
-void MostrarPersonas(char *vector[],int tam){
-    printf("Nombres almacenados: \n");
-    for(int i=0; i< tam; i++){
-        printf("Persona %d: %s\n", i+1 , vector[i]);
+void MostrarPersonas(char *nom[] , int cant){
+
+    printf("El nombre de las personas es :\n");
+    for (int i = 0; i < cant; i++)
+    {
+        printf("%s\n",nom[i]);
     }
+    
+}
+
+void BuscarNombrePorId(char *nom[], int can , int numEntero){
+    if (numEntero < can && numEntero >= 0)
+    {
+        for (int i = 0; i < can; i++)
+        {
+            if (i == numEntero)
+            {
+                printf("\nel nombre buscado por ID es :%s", nom[numEntero]);
+            }
+        
+        }
+    }else
+    {
+        printf("\nno se encontro nombre con el id ingresado");
+    }
+        
 }
