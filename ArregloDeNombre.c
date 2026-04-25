1a81fccec293ab720532045ca3587287bfab2ce1
jimeaybar <jimeaybar1999@gmail.com>
2026-04-24 22:08:02 -0300

    Merge resuelto

--- ArregloDeNombre.c
+++ ArregloDeNombre.c
@@ -7,6 +7,9 @@ void MostrarPersonas(char *nom[] , int cant);
 int buscarNombrePorClave(char *nom[], int cant , char *frase);
 
 int main(){
+
+
+
     int cantNombres = 5;
     char buff[TAMA];
     char *nombres[cantNombres];
@@ -22,13 +25,19 @@ int main(){
     }
     
     MostrarPersonas(nombres , cantNombres);
-    
-    int numfuncion;
 
+    int numfuncion;
+    printf("\nPara buscar nombre por id ingrese el numero 1:\n");
     printf("Para buscar nombre por clave ingrese el numero 2:");
     scanf("%d", &numfuncion);
     if (numfuncion == 1)
-     {
+    {   int id;
+
+        printf("\ningrese un numero entero positivo:");
+        scanf("%d", &id);
+        BuscarNombrePorId(nombres, cantNombres , id);
+    }else
+    {
         char clave[TAMA];
         printf("\ningrese un palabra clave:");
         scanf("%s", &clave);
@@ -47,8 +56,28 @@ void MostrarPersonas(char *nom[] , int cant){
     for (int i = 0; i < cant; i++)
     {
         printf("%s\n",nom[i]);
-    }   
+    }
+    
 }
+
+void BuscarNombrePorId(char *nom[], int can , int numEntero){
+    if (numEntero < can && numEntero >= 0)
+    {
+        for (int i = 0; i < can; i++)
+        {
+            if (i == numEntero)
+            {
+                printf("\nel nombre buscado por ID es :%s", nom[numEntero]);
+            }
+        
+        }
+    }else
+    {
+        printf("\nno se encontro nombre con el id ingresado");
+    }
+        
+}
+
 int buscarNombrePorClave(char *nom[], int cant , char *frase){
 
     for (int i = 0; i < cant; i++)
